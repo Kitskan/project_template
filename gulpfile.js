0b465f41029c807f9b8957262d7ae9902e295df3
Kitskan <arthurkitskan@gmail.com>
2016-07-01 22:58:47 +0300

    Project template

---
+++ gulpfile.js
@@ -0,0 +1,11 @@
+var gulp = require('gulp');
+var sass = require('gulp-sass');
+gulp.task('default', function() {
+    // place code for your default task here
+});
+
+gulp.task('sass', function () {
+    return gulp.src('app/sass/style.scss')
+        .pipe(sass().on('error', sass.logError))
+        .pipe(gulp.dest('dist/css'));
+});
