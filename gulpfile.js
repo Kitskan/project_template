19c96cb24a5eab0282b26eeb64ce630978c57c9c
Kitskan <arthurkitskan@gmail.com>
2016-07-01 23:17:29 +0300

    Project template

--- gulpfile.js
+++ gulpfile.js
@@ -1,5 +1,6 @@
 var gulp = require('gulp');
 var sass = require('gulp-sass');
+var autoprefixer = require('gulp-autoprefixer');
 gulp.task('default', function() {
     // place code for your default task here
 });
@@ -8,4 +9,15 @@ gulp.task('sass', function () {
     return gulp.src('app/sass/style.scss')
         .pipe(sass().on('error', sass.logError))
         .pipe(gulp.dest('dist/css'));
+});
+gulp.task('autoprefixer', function () {
+    return gulp.src('dist/css/style.css')
+        .pipe(autoprefixer({
+            browsers: ['last 2 versions'],
+            cascade: false
+        }))
+        .pipe(gulp.dest('dist/css/'))
+});
+gulp.task('watch', function () {
+    gulp.watch('app/sass/style.scss', ['sass']);
 });
