fa1d27ec85f4e24ed4c7d571f9ca5c239b5769fc
Kitskan <arthurkitskan@gmail.com>
2016-07-02 17:26:00 +0300

    Project template

--- gulpfile.js
+++ gulpfile.js
@@ -2,18 +2,8 @@ var gulp = require('gulp');
 var sass = require('gulp-sass');
 var autoprefixer = require('gulp-autoprefixer');
 var minifyCSS = require('gulp-minify-css');
+var rename = require("gulp-rename");
 
-
-gulp.task('default', function() {
-    return gulp.src('dist/css/style.css')
-        .pipe(autoprefixer({
-            browsers: ['last 2 versions'],
-            cascade: false
-        }))
-        .pipe(minifyCSS())
-        
-        .pipe(gulp.dest('dist/css/'))
-});
 
 gulp.task('sass', function () {
     return gulp.src('app/sass/style.scss')
@@ -21,6 +11,19 @@ gulp.task('sass', function () {
         .pipe(gulp.dest('dist/css'));
 });
 
+gulp.task('css', function() {
+    return gulp.src('dist/css/style.css')
+        .pipe(autoprefixer({
+            browsers: ['last 2 versions'],
+            cascade: false
+        }))
+        .pipe(minifyCSS())
+        .pipe(rename("style.min.css"))
+        .pipe(gulp.dest('dist/css/'))
+});
+
 gulp.task('watch', function () {
     gulp.watch('app/sass/style.scss', ['sass']);
+    gulp.watch('dist/css/style.css', ['css']);
 });
+gulp.task('default', ['watch']);
