222819cb1869166e280ee9df9728666ab10ce0dd
Kitskan <arthurkitskan@gmail.com>
2016-07-09 12:33:15 +0300

    Edit project structure

--- gulpfile.js
+++ gulpfile.js
@@ -11,51 +11,51 @@ var plumber = require('gulp-plumber');
 var browserSync = require('browser-sync').create();
 /*Script*/
 gulp.task('script', function () {
-    gulp.src('app/js/**/*.js')
+    gulp.src('src/js/**/*.js')
         .pipe(uglify())
         .pipe(rename({suffix:'.min'}))
-        .pipe(gulp.dest('dist/js/'))
+        .pipe(gulp.dest('src/js/'))
 });
 /*Sass*/
 gulp.task('sass', function () {
     return gulp.src('app/sass/style.scss')
         .pipe(sass().on('error', sass.logError))
         .pipe(plumber())
-        .pipe(gulp.dest('dist/css/'));
+        .pipe(gulp.dest('src/css/'));
 });
 /*CSS*/
 gulp.task('css', function() {
-    return gulp.src('dist/css/style.css')
+    return gulp.src('src/css/style.css')
         .pipe(autoprefixer({
             browsers: ['last 2 versions', '> 3%','ie 6-8'],
             cascade: false
         }))
         .pipe(gcmq())
-        .pipe(gulp.dest('dist/css/'))
+        .pipe(gulp.dest('src/css/'))
         .pipe(minifyCSS())
         .pipe(rename("style.min.css"))
-        .pipe(gulp.dest('dist/css/'))
+        .pipe(gulp.dest('src/css/'))
 });
 /*Html*/
 gulp.task('html', function () {
-    gulp.src('app/**/*.html')
+    gulp.src('src/**/*.html')
         .pipe(htmlAutoprefixer())
-        .pipe(gulp.dest('dist/'))
+        .pipe(gulp.dest('src/'))
 });
 /*Build*/
 gulp.task('build', ['script', 'sass', 'css', 'html']);
 /*Watch*/
 gulp.task('watch', function () {
-    gulp.watch('app/sass/style.scss', ['sass']);
-    gulp.watch('dist/css/style.css', ['css']);
-    gulp.watch('app/js/**/*.js', ['script']);
-    gulp.watch('dist/**/*.html', ['html']);
+    gulp.watch('src/sass/style.scss', ['sass']);
+    gulp.watch('src/css/style.css', ['css']);
+    gulp.watch('src/js/**/*.js', ['script']);
+    gulp.watch('src/**/*.html', ['html']);
 });
 /*Serv*/
 gulp.task('serv', function(){
   browserSync.init({
-    server: 'dist'
+    server: 'src'
   });
-  browserSync.watch('dist/**/*.*').on('change', browserSync.reload);
+  browserSync.watch('src/**/*.*').on('change', browserSync.reload);
 });
 gulp.task('default', ['build' ,'watch', 'serv']);
